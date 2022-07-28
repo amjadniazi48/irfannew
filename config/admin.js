c91b560e16042bfe84918d6ae779a22a1830d3ae
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2022-07-28 07:02:17 +0500

    install graphql plugin

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', '2jPXsBGHWJBO1JgepWUPWA=='),
+  },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'QVFxy/NZMYMMdnlYZnwFZA=='),
   },
 });
